1e75450a1af5b41a63ee50bd4152e60e514ffed1
Casey Gruppioni <casey@appcues.com>
2017-10-09 21:15:01 -0400

    Initial Commit

---
+++ QuickstartApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QuickstartApp
+//
+//  Created by Casey gruppioni on 10/9/17.
+//  Copyright © 2017 Casey gruppioni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QuickstartApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QuickstartApp
+//
+//  Created by Casey gruppioni on 10/9/17.
+//  Copyright © 2017 Casey gruppioni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
